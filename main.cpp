2bec26d04851ac1b4bebe91b38d6714ac7e2d017
Greg <gmercer10@my.bcit.ca>
2018-09-13 11:24:50 -0700

    Added gcd

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
+#include "gcd.h"
 #include <iostream>
 
+using namespace std;
+
+int gcd (int a , int b) {
+    if (b == 0) {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+
 int main() {
-    std::cout << "Hello, CLion!" << std::endl;
+
+    cout << "The greatest common denominator of 10 and 100 is " << gcd(10, 100) << endl;
+
     return 0;
 }
